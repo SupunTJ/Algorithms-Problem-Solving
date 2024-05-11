bf928e9ad9ab40cde3d861b7785c728165b326be
SupunTJ <jayaweeraspn@gmail.com>
2024-05-11 11:52:50 +0530

    bacon problem

---
+++ Contest 1/Bacon.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<char> details;
+    map<string, string> orders;
+
+    int input;
+    cin >> input;
+
+    for (int i = 0; i < input + 1; i++)
+    {
+        string s;
+        getline(cin,s);
+        
+    }
+    cout << endl;
+
+    return 0;
+}
