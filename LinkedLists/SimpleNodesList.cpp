a91d9511357237b89618393cfdaaac235a0aa35c
SupunTJ <jayaweeraspn@gmail.com>
2024-05-18 22:53:19 +0530

    Implementing a basic linkedlist

---
+++ LinkedLists/SimpleNodesList.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<cstdio>
+#include<string>
+using namespace std;
+
+
+class Node
+{
+
+public:
+
+    int data;
+    Node* next;
+
+    Node(){
+        this->data = 0;
+        this->next = nullptr;
+    }
+
+    Node(int data, Node* next){
+        this->data = data;
+        this->next = next;
+    }
+    
+};
+
+
+
+
+int main(){
+
+    Node* n1 = new Node();
+    Node* n2 = new Node(1, n1);
+    Node* n3 = new Node(2, n2);
+    Node* n4 = new Node(3, n3);
+    Node* n5 = new Node(4, n4);
+
+    Node* pointer = n5;
+
+    while (pointer != nullptr){
+        cout << pointer->data << endl;
+        pointer = pointer->next;
+    }
+
+    return 0;
+}
